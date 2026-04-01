86393a6f431f2f4a65de9f21d5a556bd155360d8
Martin Ritz <platforms@caramelocreative.cz>
2026-04-01 17:32:29 +0200

    clean urls fix

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,7 +5,7 @@ class SiteHeader extends HTMLElement {
     this.innerHTML = `
       <header class="site-header">
         <div class="wrapper">
-          <a href="/index.html" class="site-header__logo">
+          <a href="/" class="site-header__logo">
             <div class="site-header__logo--text">PRIVAT<span class="forward__slash"> / </span>INN</div>
           </a>
 
--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,12 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        contact: resolve(__dirname, './src/pages/contacts/kontakt.html'),
-        reservations: resolve(__dirname, './src/pages/reservations/rezervace.html'),
-        pricing: resolve(__dirname, './src/pages/pricing/cenik.html'),
-        rooms: resolve(__dirname, './src/pages/rooms/pokoje.html'),
-        gallery: resolve(__dirname, './src/pages/gallery/galerie.html'),
-        tips: resolve(__dirname, './src/pages/tips/tipy.html'),
+        contact: resolve(__dirname, 'src/pages/contacts/kontakt.html'),
+        reservations: resolve(__dirname, 'src/pages/reservations/rezervace.html'),
+        pricing: resolve(__dirname, 'src/pages/pricing/cenik.html'),
+        rooms: resolve(__dirname, 'src/pages/rooms/pokoje.html'),
+        gallery: resolve(__dirname, 'src/pages/gallery/galerie.html'),
+        tips: resolve(__dirname, 'src/pages/tips/tipy.html'),
       }
     }
   }
